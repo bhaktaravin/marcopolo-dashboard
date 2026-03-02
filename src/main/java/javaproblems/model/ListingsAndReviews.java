a65d5ea2c91206ab905f293adca39c462f57b246
Ravin Bhakta <ravin.bhakta@gmail.com>
2026-03-02 13:32:19 -0800

    Marco Polo datasource dashboard with live Snowflake charts and animated counters

---
+++ src/main/java/javaproblems/model/ListingsAndReviews.java
@@ -0,0 +1,19 @@
+
+package javaproblems.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+
+
+
+@Getter
+@Setter
+@Document(collection = "listingsAndReviews")
+public class ListingsAndReviews {
+
+
+    
+}
